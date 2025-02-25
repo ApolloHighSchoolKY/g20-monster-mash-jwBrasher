e5f41e5ca16bde4ca6b8bf535136d55a8061d863
jwBrasher <john.brasher@stu.daviess.kyschools.us>
2025-02-25 15:11:04 +0000

    LateNightShowHost added John Brasher

---
+++ LateNightShowHost.java
@@ -0,0 +1,11 @@
+public class LateNightShowHost extends Monster {
+    
+    public LateNightShowHost(String name){
+        super(name);
+    }
+
+    public String party(){
+        return "";
+    }
+
+}
